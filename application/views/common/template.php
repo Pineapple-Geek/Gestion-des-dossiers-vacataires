661bac06eed5b7c353f0c56a6b8bc27f794685e6
Pineapple-Geek <naim.jebbour@outlook.fr>
2019-04-24 11:17:33 +0200

    Mise en place du Template

---
+++ application/views/common/template.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url("/assets"); ?>/css/main.css">
+	<link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
+</head>
+<body>
+
+	<!--<span class="grille3">
+
+		<section ><p ><img class="logo" src="http://www.greta-npdc.fr/wp-content/themes/greta/application/images/logo/greta_gip.png"></p></section>
+		<section class="titre" ><p><h1>Fiche de renseignement personnel</h1></p></section>
+		<section class="logo2"><p><img src="http://www.greta-npdc.fr/wp-content/themes/greta/application/images/logo/greta_gip.png"></p></section>
+	</span>-->
+	<span class="tête">
+		<section class="logo"><img class="logo" src="<?php echo base_url("/assets"); ?>/images/logogreta.png"></section>
+		<section class="titre"><h1>FICHE DE RENSEIGNEMENT PERSONNEL</h1></section>
+		<section class="logo2"><img class="logo2" src="<?php echo base_url("/assets"); ?>/images/logoprogre.png"></section>
+	</span>
+
+	<!--<div id="progression">
+	<a href="formulaire1.html"> <input type="button" value="Identité et formation" class="button" style="width: 35%; height: 50px;margin-left: -230px "></a>
+	<a href="Contrat.html"> <input type="button" value="Contrat et statut" class="button" style="width: 35%; height: 50px;margin-left: 30px"></a>
+	<a href="piece.html"> <input type="button" value="Pièces jointes et validation" class="button" style="width: 35%; height: 50px;margin-left: 30px "></a>
+	</div>-->
+	<div class="btn">
+		<a href="" class="button"><p>Identité et formation</p></a>
+		<a href="" class="button"><p>Contrat et statut</p></a>
+		<a href="" class="button"><p>Pièces jointes et validation</p></a>
+	</div>
+
+<!-- <script src="piece.js" type="text/javascript"></script> -->
+</body>
+</html>
