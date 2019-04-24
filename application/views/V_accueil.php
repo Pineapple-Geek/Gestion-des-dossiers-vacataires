eff77de281fddb8e217c39784036ee6e9d0b28ee
Pineapple-Geek <naim.jebbour@outlook.fr>
2019-04-24 13:59:29 +0200

    Update Template

--- application/views/V_accueil.php
+++ application/views/V_accueil.php
@@ -1,60 +1,85 @@
-<section class="cadre">
-<div class="grille2">
-		<section>
-			<p><img class="valider" src="<?php echo base_url("/assets/images/"); ?>valider.png"></p>
-			<p><img class="valider" src="<?php echo base_url("/assets/images/"); ?>valider.png"></p>
-			<p><img class="valider" src="<?php echo base_url("/assets/images/"); ?>valider.png"></p>
-			<p><img class="valider" src="<?php echo base_url("/assets/images/"); ?>valider.png"></p>
-		</section>
+<div>
+	<section class="section">
+		<header class="header">
+			<h2 class="Ih2">Identité </h2>
+		</header>
+		<article class="article">
+			<form>
+			<aside class="aside">
+				<label id="Civilité">Civilité :</label> <input class="input" type="checkbox">Madame <input class="input" type="checkbox">Monsieur <br><br>
+				<label for="nom">Nom:</label> <input class="input" type="text" name="nom" id="nom" required><br><br>
+				<label for ="nomNaiss">Nom de naissance:</label> <input class="input" type="text" name="nomNaiss" id="nomNaiss" required><br><br>
+				<label for="prenom">Prénom:</label> <input class="input" type="text" name="prenom" id="prenom" required> <br><br>
+				<label class="label">Date de Naissance:</label> <input class="input" class="date" type="Date"> <br><br>
+				<label class="label">Lieu de Naissance:</label> <input class="input" type="text" name="fullname"> <br><br>
+				<label class="label">Département de Naissance:</label> <input class="input" type="text" name="fullname">
 
-		<section>
-			<p class="detail">Un relevé d'identité bancaire ou postal</p>
+				<h4><u> Lieu d'affectation principal :</u> </h4>
+				<label class="label">Etablissement:</label> <input class="input" type="text" name="fullname"> <br><br>
+			</aside>
+			
 
-			<p class="detail">Une copie de l'attestation de sécurité sociale</p>
-
-			<p class="detail">Une copie recto verso de la carte d'identité</P>
-
-			<p class="detail">Un curriculum mis à jour</P>
-
- 		</section>
-
-		<section>
-			<p> <input class="ajouter" type="file"></p>
-			<p><input class="ajouter" type="file"></p>
-			<p><input class="ajouter" type="file"></p>
-			<p><input class="ajouter" type="file"></p>
-		</section>
-</div>
-
-
-<section class="info">
-		Les informations receuillies font l'objet d'un traitement informatique déstiné à rédiger votre contrat de travail et assurer le suivi de l'activité du GRETA.
- 		Les destinataires des données sont le service de paye; le service des ressources humaines.
-		Conformément à la loi "informatique et libertés" , vous bénéficiez d'un droit d'accés et de rectification aux informations qui vous concernent.
-		Si vous souhaitez exercer ce droit et obtenir communication des informartions vous concernant, veuillez vous adresser au service ressources humaines.
-		Vous pouvez également, pour des motifs légitimes , vous opposez au traitement des donées vous concernant.
-</section>
-
-	<section class="fin">
-		<aside class="checkbox">
-			 <input type="checkbox" name="checkbox" value="checkbox">En cochant cette case je déclare sur l'honneur l'exactitude  des renseignements que j'ai fournis.
-		<adside><br>
-
-		<aside class="soussigne">
-			<label> Je soussigné(e): </label><br><br>
-			<label class="date"> Date: </label><br>
+		</article>
+		<article class="position">
+			<aside class="aside">
+				<label class="label">Adresse:</label> <input class="input" type="text" name="fullname"> <br><br>
+				<label class="label">Ville:</label> <input class="input" type="text" name="fullname"> <br><br>
+				<label class="label">Code Postal:</label> <input class="input" type="text" name="fullname"> <br><br>
+				<label class="label">Pays:</label> <input class="input" type="text" name="fullname"> <br><br>
+				<label class="label">N°Tel:</label> <input class="input" type="text" name="fullname"> <br><br>
+				<div id="ville">
+				<label class="label">Ville:</label> <input class="input" type="text" name="fullname"> <br><br>
+			</div>
+			</aside>
+		</article>
+		<article class="position"> 
+		<aside class="aside">
+				<label class="label">N°INSEE + CLE:</label> <input class="input" type="text" name="fullname"> <br><br>
+				<label class="label">Date de début d'activité:</label> <input class="input" class="date"type="date"> <br><br>
+				<label class="label">Adresse mail académique:</label> <input class="input" type="text" name="fullname"> <br><br>
+				<label class="label">Adresse mail perso:</label> <input class="input" type="text" name="fullname"> <br><br>
+				<label class="label">Nombre d'enfants:</label> <input class="input" type="text" name="fullname" >
+				<div id="cpostal">
+				<label class="label">Code Postal :</label> <input class="input" type="text" name="fullname">
+			</div>
 		</aside>
-		<aside>
-			<input class="signature" type="texte" name="nom et prenom"><br><br>
-			<input class="signature" type="texte" name="date">
-		</aside>
+
+			</article>
 	</section>
-</section>
 
-	<footer>
-		<section>
-			<input type="submit" value="Valider" id="bouton1">
-			<a href="Contrat.html"><input type="submit" value="Précédent" id="bouton2"></a>
-		</section>
 
-	</footer>
+
+
+	<section class="section">
+		<header class="header">
+			<h2 class="Ih2">Formation initiale et continue </h2>
+		</header>
+		<article class="article2 diplome">
+				<label class="position2">Diplôme obtenu :</label>
+				<select class="position2"name="select"> 
+					<option>Aucun diplôme (Code 8 - Niv 5bis)</option>
+					<option>Beta</option>
+					<option>Delta</option>
+				</select> <br> <br>
+				<label class="label">Intitulé du dipôme :</label> <input class="input" class="position3" type="text" name="fullname" size="26"> <br><br>
+
+
+		</article>
+		<article class="article2">
+			<p class="format"><u>Formation initiale : </u></p>
+			<label class="label">Du</label>  <input class="input" class="date" type="date" ><label class="label"> Au </label><input class="input" class="date" type="date"> <br><br>
+			<label class="label">Du</label> <input class="input" class="date" type="date" > <label class="label">Au</label> <input class="input" class="date" type="date"> <br><br>
+			<label class="label">Du</label><input class="input" class="date" type="date" ><label class="label"> Au </label><input class="input" class="date" type="date"> <br><br>
+		</article>
+		<article class="article2 formation">
+			<label class="label">Intitulé de la formation :</label> <input class="input" type="text" name="fullname"> <br><br>
+			<label class="label">Intitulé de la formation :</label> <input class="input" type="text" name="fullname"> <br><br>
+			<label class="label">Intitulé de la formation :</label> <input class="input" type="text" name="fullname"> <br><br>
+		</article>
+	</section>
+
+
+ 	<input id="bouton" type="submit" value="Suivant">
+
+  </form>
+</div>
