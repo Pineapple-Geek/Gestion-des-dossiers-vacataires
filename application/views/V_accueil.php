63e1212ab66a9708f07934f23143658639ba9d13
AntoineC59 <antoine59165@gmail.com>
2019-04-25 15:36:04 +0200

    Modif

--- application/views/V_accueil.php
+++ application/views/V_accueil.php
@@ -1,80 +1,80 @@
 <div class="row">
-<div class="card">
-<div class="card-header cardcolor"><span class="cardTitle">Identité</span></div>
+    <div class="card">
+        <div class="card-header cardcolor"><span class="cardTitle">Identité</span></div>
 
-    <div class="card-body">
-    	<form class="form-horizontal">
-    	<div class="form-row justify-content-center">
-			<div class="col bg-light">
-				<label id="Civilité" class="col-sm-6 col-form-label text-right">Civilité :</label> <input class="input" type="checkbox">Madame</input> <input class="input" type="checkbox">Monsieur</input>
-				<label for="nom" class="col-sm-6 col-form-label text-right">Nom:</label> <input class="input" type="text" name="nom" id="nom" required>
-				<label for ="nomNaiss" class="col-sm-6 col-form-label text-right">Nom de naissance:</label> <input class="input" type="text" name="nomNaiss" id="nomNaiss" required>
-				<label for="prenom" class="col-sm-6 col-form-label text-right">Prénom:</label> <input class="input" type="text" name="prenom" id="prenom" required> 
-				<label class="col-sm-6 col-form-label text-right">Date de Naissance:</label> <input class="input" class="date" type="Date"> 
-				<label class="col-sm-6 col-form-label text-right">Lieu de Naissance:</label> <input class="input" type="text" name="fullname"> 
-				<label class="col-sm-6 col-form-label text-right">Département de Naissance:</label> <input class="input" type="text" name="fullname">
-				<br><br>
-				<p class="lieuAffect"><u> Lieu d'affectation principal :</u></p>
-				<label class="col-sm-6 col-form-label text-right">Etablissement:</label> <input class="input" type="text" name="fullname">
-			</div>
-			
-			<div class="col bg-light">
-				<label class="col-sm-4 col-form-label text-right labelMarginTop">Adresse:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-4 col-form-label text-right">Ville:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-4 col-form-label text-right">Code Postal:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-4 col-form-label text-right">Pays:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-4 col-form-label text-right">N°Tel:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-4 col-form-label text-right labelMarginBottom">Ville:</label> <input class="input" type="text" name="fullname">
-			</div>
+        <div class="card-body">
+            <form class="form-horizontal">
+                <div class="form-row justify-content-center">
+                    <div class="col bg-light">
+                        <label id="Civilité" class="col-sm-6 col-form-label text-right">Civilité :</label> <input class="input" type="checkbox">Madame</input> <input class="input" type="checkbox">Monsieur</input>
+                        <label for="nom" class="col-sm-6 col-form-label text-right">Nom:</label> <input class="input" type="text" name="nom" id="nom" required>
+                        <label for ="nomNaiss" class="col-sm-6 col-form-label text-right">Nom de naissance:</label> <input class="input" type="text" name="nomNaiss" id="nomNaiss" required>
+                        <label for="prenom" class="col-sm-6 col-form-label text-right">Prénom:</label> <input class="input" type="text" name="prenom" id="prenom" required>
+                        <label class="col-sm-6 col-form-label text-right">Date de Naissance:</label> <input class="input" class="date" type="Date">
+                        <label class="col-sm-6 col-form-label text-right">Lieu de Naissance:</label> <input class="input" type="text" name="fullname">
+                        <label class="col-sm-6 col-form-label text-right">Département de Naissance:</label> <input class="input" type="text" name="fullname">
+                        <br><br>
+                        <p class="lieuAffect"><u> Lieu d'affectation principal :</u></p>
+                        <label class="col-sm-6 col-form-label text-right">Etablissement:</label> <input class="input" type="text" name="fullname">
+                    </div>
 
-			<div class="col bg-light">
-				<label class="col-sm-6 col-form-label text-right labelMarginTop">N°INSEE + CLE:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-6 col-form-label text-right">Date de début d'activité:</label> <input class="input" class="date"type="date">
-				<label class="col-sm-6 col-form-label text-right">Adresse mail académique:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-6 col-form-label text-right">Adresse mail perso:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-6 col-form-label text-right">Nombre d'enfants:</label> <input class="input" type="text" name="fullname" >
-				<label class="col-sm-6 col-form-label text-right labelMarginBottom">Code Postal :</label> <input class="input" type="text" name="fullname">
-			</div>
-		</div>
-	</div>
-</div>
-<div class="card cardMargin">
-	<div class="card-header cardcolor"><span class="cardTitle">Formation initiale et continue</span></div>
+                    <div class="col bg-light">
+                        <label class="col-sm-4 col-form-label text-right labelMarginTop">Adresse:</label> <input class="input" type="text" name="fullname">
+                        <label class="col-sm-4 col-form-label text-right">Ville:</label> <input class="input" type="text" name="fullname">
+                        <label class="col-sm-4 col-form-label text-right">Code Postal:</label> <input class="input" type="text" name="fullname">
+                        <label class="col-sm-4 col-form-label text-right">Pays:</label> <input class="input" type="text" name="fullname">
+                        <label class="col-sm-4 col-form-label text-right">N°Tel:</label> <input class="input" type="text" name="fullname">
+                        <label class="col-sm-4 col-form-label text-right labelMarginBottom">Ville:</label> <input class="input" type="text" name="fullname">
+                    </div>
 
-	    <div class="card-body">
-	    	<div class="form-row justify-content-center">
+                    <div class="col bg-light">
+                        <label class="col-sm-6 col-form-label text-right labelMarginTop">N°INSEE + CLE:</label> <input class="input" type="text" name="fullname">
+                        <label class="col-sm-6 col-form-label text-right">Date de début d'activité:</label> <input class="input" class="date"type="date">
+                        <label class="col-sm-6 col-form-label text-right">Adresse mail académique:</label> <input class="input" type="text" name="fullname">
+                        <label class="col-sm-6 col-form-label text-right">Adresse mail perso:</label> <input class="input" type="text" name="fullname">
+                        <label class="col-sm-6 col-form-label text-right">Nombre d'enfants:</label> <input class="input" type="text" name="fullname" >
+                        <label class="col-sm-6 col-form-label text-right labelMarginBottom">Code Postal :</label> <input class="input" type="text" name="fullname">
+                    </div>
+                </div>
+        </div>
+    </div>
+    <div class="card cardMargin">
+        <div class="card-header cardcolor"><span class="cardTitle">Formation initiale et continue</span></div>
 
-				<div class="col bg-light">
-					<label class="col-sm-4 col-form-label text-right labelMarginTop2">Diplôme obtenu :</label>
-					<select name="select"> 
-						<option>Aucun diplôme (Code 8 - Niv 5bis)</option>
-						<option>Beta</option>
-						<option>Delta</option>
-					</select>
-					<label class="col-sm-4 col-form-label text-right">Intitulé du dipôme :</label> <input class="input" type="text" name="fullname" size="26"> 
-				</div>
+        <div class="card-body">
+            <div class="form-row justify-content-center">
 
-				<div class="col bg-light">
-				<p><u>Formation initiale : </u></p>
-				<label>Du</label> <input class="input" class="date" type="date" ><label class="text-Date">Au</label><input class="input" class="date" type="date"><br> 
-				<label>Du</label> <input class="input" class="date" type="date" ><label class="text-Date">Au</label><input class="input" class="date" type="date"><br> 
-				<label>Du</label> <input class="input" class="date" type="date" ><label class="text-Date">Au</label><input class="input" class="date" type="date"><br> 
-				</div>
+                <div class="col bg-light">
+                    <label class="col-sm-4 col-form-label text-right labelMarginTop2">Diplôme obtenu :</label>
+                    <select name="select">
+                        <option>Aucun diplôme (Code 8 - Niv 5bis)</option>
+                        <option>Beta</option>
+                        <option>Delta</option>
+                    </select>
+                    <label class="col-sm-4 col-form-label text-right">Intitulé du dipôme :</label> <input class="input" type="text" name="fullname" size="26">
+                </div>
 
-				<div class="col bg-light">
-				<label class="col-sm-5 col-form-label text-right labelMarginTop3">Intitulé de la formation :</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-5 col-form-label text-right">Intitulé de la formation :</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-5 col-form-label text-right">Intitulé de la formation :</label> <input class="input" type="text" name="fullname">
-				</div>
-			</div>
-		</div>
-	</div>
+                <div class="col bg-light">
+                    <p><u>Formation initiale : </u></p>
+                    <label>Du</label> <input class="input" class="date" type="date" ><label class="text-Date">Au</label><input class="input" class="date" type="date"><br>
+                    <label>Du</label> <input class="input" class="date" type="date" ><label class="text-Date">Au</label><input class="input" class="date" type="date"><br>
+                    <label>Du</label> <input class="input" class="date" type="date" ><label class="text-Date">Au</label><input class="input" class="date" type="date"><br>
+                </div>
+
+                <div class="col bg-light">
+                    <label class="col-sm-5 col-form-label text-right labelMarginTop3">Intitulé de la formation :</label> <input class="input" type="text" name="fullname">
+                    <label class="col-sm-5 col-form-label text-right">Intitulé de la formation :</label> <input class="input" type="text" name="fullname">
+                    <label class="col-sm-5 col-form-label text-right">Intitulé de la formation :</label> <input class="input" type="text" name="fullname">
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <div class="row">
-	<div class="col-sm-1 offset-sm-11">
-		<input type="submit" value="Suivant" class="inputcolor">
-	</div>
+    <div class="col-sm-1 offset-sm-11">
+        <input type="submit" value="Suivant" class="inputcolor">
+    </div>
 </div>
 
 </form>
--- application/views/V_administrateur.php
+++ application/views/V_administrateur.php
@@ -1,17 +1,42 @@
-
-        <div class="card">
-            <div class="card-header cardcolor">
-                <span class="cardTitle">Administration</span>
-            </div>
-            <div class="card-body">
-                <nav class="navbar navbar-light bg-gradient-secondary btn">
-                    <a href="" class="btn btn-success"><p class="btn-text">Création de compte</p></a>
-                    <a href="" class="btn btn-success"><p class="btn-text">Modification de compte</p></a>
-                    <a href="" class="btn btn-success"><p class="btn-text">Suppression de compte</p></a>
-                </nav>
-
-            </div>
-        </div>
-
-
-
+<div class="card">
+    <div class="card-header cardcolor">
+        <span class="cardTitle">Administration</span>
+    </div>
+    <div class="card-body">
+        <nav class="navbar navbar-light bg-gradient-secondary btn">
+            <a href="" class="btn btn-success"><p class="btn-text">Création de compte</p></a>
+            <a href="" class="btn btn-success"><p class="btn-text">Modification de compte</p></a>
+            <a href="" class="btn btn-success"><p class="btn-text">Suppression de compte</p></a>
+        </nav>
+        <table class="table">
+            <thead>
+            <tr class="tableAdmin">
+                <th scope="col">Id</th>
+                <th scope="col">Nom</th>
+                <th scope="col">Prénom</th>
+                <th scope="col">Type</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Nom1</td>
+                <td>Prenom1</td>
+                <td>Agent de paie</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Nom2</td>
+                <td>Prenom2</td>
+                <td>Agent de paie</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Nom3</td>
+                <td>Prenom3</td>
+                <td>Agent de paie</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
