520403f4d6ac0b2a8d5b81f3bf774ab5b1eb94ba
Pineapple-Geek <naim.jebbour@outlook.fr>
2019-04-25 14:19:53 +0200

    Correctif V_accueil

--- application/views/V_accueil.php
+++ application/views/V_accueil.php
@@ -6,34 +6,34 @@
     	<form class="form-horizontal">
     	<div class="form-row justify-content-center">
 			<div class="col bg-light">
-				<label id="Civilité" class="col-sm-6 col-form-label">Civilité :</label> <input class="input" type="checkbox">Madame</input> <input class="input" type="checkbox">Monsieur</input>
-				<label for="nom" class="col-sm-6 col-form-label">Nom:</label> <input class="input" type="text" name="nom" id="nom" required>
-				<label for ="nomNaiss" class="col-sm-6 col-form-label">Nom de naissance:</label> <input class="input" type="text" name="nomNaiss" id="nomNaiss" required>
-				<label for="prenom" class="col-sm-6 col-form-label">Prénom:</label> <input class="input" type="text" name="prenom" id="prenom" required> 
-				<label class="col-sm-6 col-form-label">Date de Naissance:</label> <input class="input" class="date" type="Date"> 
-				<label class="col-sm-6 col-form-label">Lieu de Naissance:</label> <input class="input" type="text" name="fullname"> 
-				<label class="col-sm-6 col-form-label">Département de Naissance:</label> <input class="input" type="text" name="fullname">
+				<label id="Civilité" class="col-sm-6 col-form-label text-right">Civilité :</label> <input class="input" type="checkbox">Madame</input> <input class="input" type="checkbox">Monsieur</input>
+				<label for="nom" class="col-sm-6 col-form-label text-right">Nom:</label> <input class="input" type="text" name="nom" id="nom" required>
+				<label for ="nomNaiss" class="col-sm-6 col-form-label text-right">Nom de naissance:</label> <input class="input" type="text" name="nomNaiss" id="nomNaiss" required>
+				<label for="prenom" class="col-sm-6 col-form-label text-right">Prénom:</label> <input class="input" type="text" name="prenom" id="prenom" required> 
+				<label class="col-sm-6 col-form-label text-right">Date de Naissance:</label> <input class="input" class="date" type="Date"> 
+				<label class="col-sm-6 col-form-label text-right">Lieu de Naissance:</label> <input class="input" type="text" name="fullname"> 
+				<label class="col-sm-6 col-form-label text-right">Département de Naissance:</label> <input class="input" type="text" name="fullname">
 				<br><br>
-				<p><u> Lieu d'affectation principal :</u> </p>
-				<label class="col-sm-6 col-form-label">Etablissement:</label> <input class="input" type="text" name="fullname">
+				<p class="lieuAffect"><u> Lieu d'affectation principal :</u></p>
+				<label class="col-sm-6 col-form-label text-right">Etablissement:</label> <input class="input" type="text" name="fullname">
 			</div>
 			
 			<div class="col bg-light">
-				<label class="col-sm-4 col-form-label labelMarginTop">Adresse:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-4 col-form-label">Ville:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-4 col-form-label">Code Postal:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-4 col-form-label">Pays:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-4 col-form-label">N°Tel:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-4 col-form-label labelMarginBottom">Ville:</label> <input class="input" type="text" name="fullname">
+				<label class="col-sm-4 col-form-label text-right labelMarginTop">Adresse:</label> <input class="input" type="text" name="fullname">
+				<label class="col-sm-4 col-form-label text-right">Ville:</label> <input class="input" type="text" name="fullname">
+				<label class="col-sm-4 col-form-label text-right">Code Postal:</label> <input class="input" type="text" name="fullname">
+				<label class="col-sm-4 col-form-label text-right">Pays:</label> <input class="input" type="text" name="fullname">
+				<label class="col-sm-4 col-form-label text-right">N°Tel:</label> <input class="input" type="text" name="fullname">
+				<label class="col-sm-4 col-form-label text-right labelMarginBottom">Ville:</label> <input class="input" type="text" name="fullname">
 			</div>
 
 			<div class="col bg-light">
-				<label class="col-sm-6 col-form-label labelMarginTop">N°INSEE + CLE:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-6 col-form-label">Date de début d'activité:</label> <input class="input" class="date"type="date">
-				<label class="col-sm-6 col-form-label">Adresse mail académique:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-6 col-form-label">Adresse mail perso:</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-6 col-form-label">Nombre d'enfants:</label> <input class="input" type="text" name="fullname" >
-				<label class="col-sm-6 col-form-label labelMarginBottom">Code Postal :</label> <input class="input" type="text" name="fullname">
+				<label class="col-sm-6 col-form-label text-right labelMarginTop">N°INSEE + CLE:</label> <input class="input" type="text" name="fullname">
+				<label class="col-sm-6 col-form-label text-right">Date de début d'activité:</label> <input class="input" class="date"type="date">
+				<label class="col-sm-6 col-form-label text-right">Adresse mail académique:</label> <input class="input" type="text" name="fullname">
+				<label class="col-sm-6 col-form-label text-right">Adresse mail perso:</label> <input class="input" type="text" name="fullname">
+				<label class="col-sm-6 col-form-label text-right">Nombre d'enfants:</label> <input class="input" type="text" name="fullname" >
+				<label class="col-sm-6 col-form-label text-right labelMarginBottom">Code Postal :</label> <input class="input" type="text" name="fullname">
 			</div>
 		</div>
 	</div>
@@ -45,26 +45,26 @@
 	    	<div class="form-row justify-content-center">
 
 				<div class="col bg-light">
-					<label class="col-sm-4 col-form-label labelMarginTop2">Diplôme obtenu :</label>
+					<label class="col-sm-4 col-form-label text-right labelMarginTop2">Diplôme obtenu :</label>
 					<select name="select"> 
 						<option>Aucun diplôme (Code 8 - Niv 5bis)</option>
 						<option>Beta</option>
 						<option>Delta</option>
 					</select>
-					<label class="col-sm-4 col-form-label">Intitulé du dipôme :</label> <input class="input" type="text" name="fullname" size="26"> 
+					<label class="col-sm-4 col-form-label text-right">Intitulé du dipôme :</label> <input class="input" type="text" name="fullname" size="26"> 
 				</div>
 
 				<div class="col bg-light">
 				<p><u>Formation initiale : </u></p>
-				<label>Du</label> <input class="input" class="date" type="date" ><label>Au</label><input class="input" class="date" type="date"><br> 
-				<label>Du</label> <input class="input" class="date" type="date" ><label>Au</label><input class="input" class="date" type="date"><br> 
-				<label>Du</label> <input class="input" class="date" type="date" ><label>Au</label><input class="input" class="date" type="date"><br> 
+				<label>Du</label> <input class="input" class="date" type="date" ><label class="text-Date">Au</label><input class="input" class="date" type="date"><br> 
+				<label>Du</label> <input class="input" class="date" type="date" ><label class="text-Date">Au</label><input class="input" class="date" type="date"><br> 
+				<label>Du</label> <input class="input" class="date" type="date" ><label class="text-Date">Au</label><input class="input" class="date" type="date"><br> 
 				</div>
 
 				<div class="col bg-light">
-				<label class="col-sm-5 col-form-label labelMarginTop3">Intitulé de la formation :</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-5 col-form-label">Intitulé de la formation :</label> <input class="input" type="text" name="fullname">
-				<label class="col-sm-5 col-form-label">Intitulé de la formation :</label> <input class="input" type="text" name="fullname">
+				<label class="col-sm-5 col-form-label text-right labelMarginTop3">Intitulé de la formation :</label> <input class="input" type="text" name="fullname">
+				<label class="col-sm-5 col-form-label text-right">Intitulé de la formation :</label> <input class="input" type="text" name="fullname">
+				<label class="col-sm-5 col-form-label text-right">Intitulé de la formation :</label> <input class="input" type="text" name="fullname">
 				</div>
 			</div>
 		</div>
