045b1b6fff2fd78bdf9bb652c52f22c0bd400596
AntoineC59 <antoine59165@gmail.com>
2019-04-25 15:48:40 +0200

    modif administrateur

--- application/controllers/C_administrateur.php
+++ application/controllers/C_administrateur.php
@@ -3,7 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class C_administrateur extends CI_Controller {
 
-    public function menu()
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
     {
         $data['title'] = "Menu administrateur";
         $page = $this->load->view('V_administrateur', $data, true);
