1103cefedd161004ac28389e2cf79de3bce40cb4
AntoineC59 <antoine59165@gmail.com>
2019-04-29 15:35:42 +0200

    css + connexion

--- application/views/V_connexion.php
+++ application/views/V_connexion.php
@@ -6,9 +6,9 @@
     <div class="card-body">
         <form class="form-horizontal" action="<?php echo site_url('C_connexion/validation'); ?>" method="post">
             <div class="form-row justify-content-center">
-                <div class="col ">
-                    <label for="login" class="col-sm-6 col-form-label text-right" >Login:</label> <input class="input" type="text" name="login" id="login" placeholder="Entrer votre login" required><br>
-                    <label for="mdp" class="col-sm-6 col-form-label text-right">Mots de passe:</label> <input class="input" type="text" name="mdp" id="mdp" placeholder="Entrer votre mots de passe" required><br>
+                <div class="col rowmargin">
+                    <label for="login" class="col-sm-6 col-form-label text-right" >Identifiant:</label> <input class="input" type="text" name="login" id="login"  required><br>
+                    <label for="mdp" class="col-sm-6 col-form-label text-right">Mots de passe:</label> <input class="input" type="text" name="mdp" id="mdp" required><br>
                     <nav class="navbar navbar-light bg-gradient-secondary btn">
                         <input  class="btn btn-success" type="submit" style="margin: auto"><p class="btn-text"></p></input>
                     </nav>
