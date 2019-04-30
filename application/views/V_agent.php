17cec0d4ce92a0b41acee4183a7e40e8081b9f13
AntoineC59 <antoine59165@gmail.com>
2019-04-30 12:12:37 +0200

    Update V_agent.php

--- application/views/V_agent.php
+++ application/views/V_agent.php
@@ -1,4 +1,15 @@
 <?php if (isset($_SESSION["loginok"]) && ($_SESSION["loginok"]["type"] == "Administrateur")) { // si la session n'est pas connectée, afficher le formulaire de connexion'?>
+<div class="card">
+    <div class="card-header cardcolor">
+        <span class="cardTitle">Choix du dossier</span>
+    </div>
 
+    <div class="card-body">
+        <form class="form-inline ">
+            <input class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search" aria-label="Search">
+            <i class="fas fa-search" aria-hidden="true"></i>
+        </form>
+    </div>
+</div>
 
 <?php } else { redirect('C_connexion','refresh'); }?>
