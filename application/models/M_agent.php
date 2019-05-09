4ebc5f290156e5edfba686ec40cc52a669d0c539
AntoineC59 <antoine59165@gmail.com>
2019-05-09 11:03:52 +0200

    maj agent

--- application/models/M_agent.php
+++ application/models/M_agent.php
@@ -9,5 +9,20 @@ class M_agent extends CI_Model
         parent::__construct(); // Call the CI_Model constructor
         $this->load->database();
     }
+    public function select_identite()
+    {
+        $query = $this->db->select('*')
+            ->from('identite')
+            ->get();
+        return $query->result_array(); //conversion en tableau PH
+    }
+
+    public function select_verification()
+    {
+        $query = $this->db->select('*')
+            ->from('verification')
+            ->get();
+        return $query->result_array(); //conversion en tableau PH
+    }
 
 }
