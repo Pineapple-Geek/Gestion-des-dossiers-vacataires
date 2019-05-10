fb06d8efc8edfc080d4f51c1b0c7eea1e56fa6b1
Pineapple-Geek <naim.jebbour@outlook.fr>
2019-05-10 16:48:10 +0200

    Update V_detail.php

--- application/views/V_detail.php
+++ application/views/V_detail.php
@@ -7,35 +7,35 @@
                 <div class="form-row justify-content-center">
                     <div class="col">
                         <label class="col-sm-6 col-form-label text-right">Nom:</label> <label class="col-sm-4 col-form-label text-left"><?php echo $identite[0]["Nom"]; ?></label>
-                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation1" type="radio" value="Madame" required>Valide</input> <input name="validation1" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation1" type="radio" value="Valide" required>Valide</input> <input name="validation1" type="radio" value="NonValide" required>Non Valide</input></span>
                         <label class="col-sm-6 col-form-label text-right">Nom de naissance:</label> <label class="col-sm-4 col-form-label text-left"><?php echo $identite[0]["Nom_Naissance"]; ?></label>
-                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation2" type="radio" value="Madame" required>Valide</input> <input name="validation2" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation2" type="radio" value="Valide" required>Valide</input> <input name="validation2" type="radio" value="NonValide" required>Non Valide</input></span>
                         <label class="col-sm-6 col-form-label text-right">Prénom:</label> <label class="col-sm-4 col-form-label text-left"><?php echo $identite[0]["Prenom"]; ?></label>
-                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation3" type="radio" value="Madame" required>Valide</input> <input name="validation3" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation3" type="radio" value="Valide" required>Valide</input> <input name="validation3" type="radio" value="NonValide" required>Non Valide</input></span>
                         <label class="col-sm-6 col-form-label text-right">Date de Naissance:</label> <label class="col-sm-4 col-form-label text-left"><?php echo $identite[0]["Date_Naissance"]; ?></label>
-                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation4" type="radio" value="Madame" required>Valide</input> <input name="validation4" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation4" type="radio" value="Valide" required>Valide</input> <input name="validation4" type="radio" value="NonValide" required>Non Valide</input></span>
                         <label class="col-sm-6 col-form-label text-right">Lieu de Naissance:</label> <label class="col-sm-4 col-form-label text-left"><?php echo $identite[0]["Lieu_Naissance"]; ?></label>
-                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation5" type="radio" value="Madame" required>Valide</input> <input name="validation5" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation5" type="radio" value="Valide" required>Valide</input> <input name="validation5" type="radio" value="NonValide" required>Non Valide</input></span>
                         <label class="col-sm-6 col-form-label text-right">Département de Naissance:</label> <label class="col-sm-4 col-form-label text-left"><?php echo $identite[0]["Departement"]; ?></label>
-                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation6" type="radio" value="Madame" required>Valide</input> <input name="validation6" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation6" type="radio" value="Valide" required>Valide</input> <input name="validation6" type="radio" value="NonValide" required>Non Valide</input></span>
                     </div>
 
                     <div class="col">
                         <label class="col-sm-4 col-form-label text-right labelMarginTop">Adresse:</label> <label class="col-sm-5 col-form-label text-left"><?php echo $identite[0]["Adresse"]; ?></label>
-                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation7" type="radio" value="Madame" required>Valide</input> <input name="validation7" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation7" type="radio" value="Valide" required>Valide</input> <input name="validation7" type="radio" value="NonValide" required>Non Valide</input></span>
                         <label class="col-sm-4 col-form-label text-right">Ville:</label> <label class="col-sm-5 col-form-label text-left"><?php echo $identite[0]["Ville"]; ?></label>
-                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation8" type="radio" value="Madame" required>Valide</input> <input name="validation8" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation8" type="radio" value="Valide" required>Valide</input> <input name="validation8" type="radio" value="NonValide" required>Non Valide</input></span>
                         <label class="col-sm-4 col-form-label text-right">Code Postal:</label> <label class="col-sm-5 col-form-label text-left"><?php echo $identite[0]["Code_Postal"]; ?></label>
-                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation9" type="radio" value="Madame" required>Valide</input> <input name="validation9" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation9" type="radio" value="Valide" required>Valide</input> <input name="validation9" type="radio" value="NonValide" required>Non Valide</input></span>
                         <label class="col-sm-4 col-form-label text-right">Pays:</label> <label class="col-sm-5 col-form-label text-left"><?php echo $identite[0]["Pays"]; ?></label>
-                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation10" type="radio" value="Madame" required>Valide</input> <input name="validation10" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation10" type="radio" value="Valide" required>Valide</input> <input name="validation10" type="radio" value="NonValide" required>Non Valide</input></span>
                         <label class="col-sm-4 col-form-label text-right">N°Tel:</label> <label class="col-sm-5 col-form-label text-left"><?php echo $identite[0]["N_Tel"]; ?></label>
-                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation11" type="radio" value="Madame" required>Valide</input> <input name="validation11" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation11" type="radio" value="Valide" required>Valide</input> <input name="validation11" type="radio" value="NonValide" required>Non Valide</input></span>
                     </div>
 
                     <div class="col">
                         <label class="col-sm-6 col-form-label text-right labelMarginTop">N° Sécurité Sociale:</label> <label class="col-sm-5 col-form-label text-left"><?php echo $identite[0]["N_Securite_sociale"]; ?></label>
-                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation12" type="radio" value="Madame" required>Valide</input> <input name="validation12" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation12" type="radio" value="Valide" required>Valide</input> <input name="validation12" type="radio" value="NonValide" required>Non Valide</input></span>
                         <label class="col-sm-6 col-form-label text-right">Adresse mail académique:</label> <label class="col-sm-5 col-form-label text-left">
                             <?php 
                             if ($identite[0]["Adresse_Mail_Academique"] != null)
@@ -48,7 +48,7 @@
                             }
                             ?></label>
                         <label class="col-sm-6 col-form-label text-right">Adresse mail perso:</label> <label class="col-sm-5 col-form-label text-left"><?php echo $identite[0]["Adresse_Mail_Perso"]; ?></label>
-                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation13" type="radio" value="Madame" required>Valide</input> <input name="validation13" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation13" type="radio" value="Valide" required>Valide</input> <input name="validation13" type="radio" value="NonValide" required>Non Valide</input></span>
                     </div>
                 </div>
         </div>
@@ -64,7 +64,7 @@
                         <h6 class="card-title cardBodyTitle">Fonction occupée : </h6>
 
                         <label class="col-sm-5 col-form-label text-right">Intervenants : </label><label class="col-sm-3 col-form-label text-left"><?php echo $contrat[0]["Intervenants"]; ?></label>
-                        <label class="col-sm-5 col-form-label text-right"></label><span class="validationStyle"><input name="validation14" type="radio" value="Madame" required>Valide</input> <input name="validation14" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-5 col-form-label text-right"></label><span class="validationStyle"><input name="validation14" type="radio" value="Valide" required>Valide</input> <input name="validation14" type="radio" value="NonValide" required>Non Valide</input></span>
                     </div>
                 </div>
                 <fieldset id="formEtat">
@@ -73,31 +73,31 @@
 
                     <div class="col"> 
                         <label class="col-sm-6 col-form-label text-right">Administratif : </label><label class="col-sm-5 col-form-label text-left"><?php echo $contrat[0]["Administratif"]; ?></label><br>
-                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation14" type="radio" value="Madame" required>Valide</input> <input name="validation14" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-6 col-form-label text-right"></label><span class="validationStyle"><input name="validation14" type="radio" value="Valide" required>Valide</input> <input name="validation14" type="radio" value="NonValide" required>Non Valide</input></span>
                     </div>
 
                     <div class="col">
                         <label class="col-sm-3 col-form-label text-right">Statut : </label><label class="col-sm-9 col-form-label text-left"><?php echo $contrat[0]["Status"]; ?></label>
-                        <label class="col-sm-3 col-form-label text-right"></label><span class="validationStyle"><input name="validation13" type="radio" value="Madame" required>Valide</input> <input name="validation13" type="radio" value="Monsieur" required>Non Valide</input></span><br>
+                        <label class="col-sm-3 col-form-label text-right"></label><span class="validationStyle"><input name="validation13" type="radio" value="Valide" required>Valide</input> <input name="validation13" type="radio" value="NonValide" required>Non Valide</input></span><br>
                     </div>
                     <div class="col">
                         <label class="col-sm-4 col-form-label text-right">Catégorie : </label><label class="col-sm-8 col-form-label text-left"><?php echo $contrat[0]["Categorie"]; ?></label>
-                        <label class="col-sm-4 col-form-label text-right"></label><span class="validationStyle"><input name="validation14" type="radio" value="Madame" required>Valide</input> <input name="validation14" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-4 col-form-label text-right"></label><span class="validationStyle"><input name="validation14" type="radio" value="Valide" required>Valide</input> <input name="validation14" type="radio" value="NonValide" required>Non Valide</input></span>
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="col">
                         <p class="lieuAffect"><u> Lieu d'affectation principal :</u></p>
                         <label class="col-sm-4 col-form-label text-right labelMarginLeft2">Etablissement:</label> <label class="col-sm-7 col-form-label text-left"><?php echo $contrat[0]["Etablissement"]; ?></label>
-                        <label class="col-sm-4 col-form-label text-right"></label><span class="validationStyle"><input name="validation14" type="radio" value="Madame" required>Valide</input> <input name="validation14" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-4 col-form-label text-right"></label><span class="validationStyle"><input name="validation14" type="radio" value="Valide" required>Valide</input> <input name="validation14" type="radio" value="NonValide" required>Non Valide</input></span>
                     </div>
                     <div class="col">
                         <label class="col-sm-3 col-form-label text-right labelMarginBottom">Ville:</label> <label class="col-sm-8 col-form-label text-left"><?php echo $contrat[0]["Ville_E"]; ?></label>
-                        <label class="col-sm-3 col-form-label text-right"></label><span class="validationStyle"><input name="validation14" type="radio" value="Madame" required>Valide</input> <input name="validation14" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-3 col-form-label text-right"></label><span class="validationStyle"><input name="validation14" type="radio" value="Valide" required>Valide</input> <input name="validation14" type="radio" value="NonValide" required>Non Valide</input></span>
                     </div>
                     <div class="col">
                         <label class="col-sm-4 col-form-label text-right labelMarginBottom">Code Postal :</label> <label class="col-sm-7 col-form-label text-left"><?php echo $contrat[0]["Code_Postal_E"]; ?></label>
-                        <label class="col-sm-4 col-form-label text-right"></label><span class="validationStyle"><input name="validation14" type="radio" value="Madame" required>Valide</input> <input name="validation14" type="radio" value="Monsieur" required>Non Valide</input></span>
+                        <label class="col-sm-4 col-form-label text-right"></label><span class="validationStyle"><input name="validation14" type="radio" value="Valide" required>Valide</input> <input name="validation14" type="radio" value="NonValide" required>Non Valide</input></span>
                     </div>
                 </div>
             </fieldset>
